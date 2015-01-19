3590dd8484098d0162eaa83477b56d36075c1195
danielloureda <Dano@MacBook-Air-de-Daniel.local>
2015-01-19 17:14:02 +0100

    Initial Commit

---
+++ Calculadora/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculadora
+//
+//  Created by Daniel Loureda Arteaga on 19/1/15.
+//  Copyright (c) 2015 Daniel Loureda Arteaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
